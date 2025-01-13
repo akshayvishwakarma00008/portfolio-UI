3bf388a8f8f8ce493af1797d09d25675d664e929
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2025-01-13 22:14:11 +0530

    updated file name

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
-import './Work.scss';
+import './work.scss';
 
 // eslint-disable-next-line react-refresh/only-export-components
 const Work = () => {
